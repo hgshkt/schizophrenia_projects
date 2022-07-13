a7ea0fab9a61c05c23ed0f7aa31c563dbb16f72c
hgshkt <nichtojestvozxc@gmail.com>
2022-07-13 18:56:43 +0300

    bug fix

--- src/Main.kt
+++ src/Main.kt
@@ -33,7 +33,7 @@ fun main() {
             field[0][1] == field[1][1] && field[1][1] == field[2][1] && field[0][1] != '_' ||       // someone
             field[0][2] == field[1][2] && field[1][2] == field[2][2] && field[0][2] != '_' ||       // victory
             field[0][0] == field[1][1] && field[1][1] == field[2][2] && field[0][0] != '_' ||       // or
-            field[0][2] == field[2][2] && field[2][2] == field[2][0] && field[0][2] != '_' ||       // tie
+            field[0][2] == field[1][1] && field[1][1] == field[2][0] && field[0][2] != '_' ||       // tie
             field.all { !it.contains('_')})                                                         //
             gameRun = false                                                                         //
 
